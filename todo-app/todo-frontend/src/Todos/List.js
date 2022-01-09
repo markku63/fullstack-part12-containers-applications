339165b32eecf60b96aa9c91fbf889cf4b4f08cd
Markku Kolkka <markku.kolkka@iki.fi>
2022-01-09 14:05:03 +0200

    Exercise 12.15

--- todo-app/todo-frontend/src/Todos/List.js
+++ todo-app/todo-frontend/src/Todos/List.js
@@ -14,6 +14,7 @@ const TodoList = ({ todos, deleteTodo, completeTodo }) => {
     <>
       {todos.map(todo =>
         <Todo
+          key={todo._id}
           todo={todo}
           onClickDelete={onClickDelete}
           onClickComplete={onClickComplete}
